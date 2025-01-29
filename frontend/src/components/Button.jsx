f6bc9f6a0429357b17a27317b74819f07def4a3b
Guru Visual 3 <guruvisual3@iMac-de-Guru-3.local>
2025-01-28 23:41:15 -0400

    🔥 Eliminando todos los archivos para subir limpio

--- frontend/src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import { Button as MuiButton } from '@mui/material';
-
-const Button = ({ children, ...props }) => {
-  return <MuiButton {...props}>{children}</MuiButton>;
-};
-
-export default Button;
--- frontend/src/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-import { TextField } from '@mui/material';
-
-const Input = ({ label, ...props }) => {
-  return <TextField label={label} fullWidth {...props} />;
-};
-
-export default Input;
